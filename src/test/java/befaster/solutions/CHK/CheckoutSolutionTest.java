ac3a3e93a3ae0de8a8d6847c45311c489700bb1d
sbx_user1051 <sbx_user1051@169.254.86.237>
2019-10-12 21:04:03 +0000

    Sat Oct 12 21:04:03 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -102,7 +102,8 @@ public class CheckoutSolutionTest {
 
         assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHQ"), equalTo(1140));
         assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHQRR"), equalTo(1210));
-        assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHQQRR"), equalTo(1210));
+        assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHQQRR"), equalTo(1240));
+        assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHQQQRR"), equalTo(1260));
 
         assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHHQQ"), equalTo(1165));
         assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHHQQQQ"), equalTo(1225));
@@ -118,45 +119,4 @@ public class CheckoutSolutionTest {
         assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHHQQQQQUUUUUVVVVVVV"), equalTo(1705));
         assertThat(checkoutSolution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZKHHHHHHHHHQQQQQUUUUUVVVVVVVV"), equalTo(1745));
     }
-
-//    Our price table and offers:
-//            +------+-------+------------------------+
-//            | Item | Price | Special offers         |
-//            +------+-------+------------------------+
-//            | A    | 50    | 3A for 130, 5A for 200 |
-//            | B    | 30    | 2B for 45              |
-//            | C    | 20    |                        |
-//            | D    | 15    |                        |
-//            | E    | 40    | 2E get one B free      |
-//            | F    | 10    | 2F get one F free      |
-//            | G    | 20    |                        |
-//            | H    | 10    | 5H for 45, 10H for 80  |
-//            | I    | 35    |                        |
-//            | J    | 60    |                        |
-//            | K    | 80    | 2K for 150             |
-//            | L    | 90    |                        |
-//            | M    | 15    |                        |
-//            | N    | 40    | 3N get one M free      |
-//            | O    | 10    |                        |
-//            | P    | 50    | 5P for 200             |
-//            | Q    | 30    | 3Q for 80              |
-//            | R    | 50    | 3R get one Q free      |
-//            | S    | 30    |                        |
-//            | T    | 20    |                        |
-//            | U    | 40    | 3U get one U free      |
-//            | V    | 50    | 2V for 90, 3V for 130  |
-//            | W    | 20    |                        |
-//            | X    | 90    |                        |
-//            | Y    | 10    |                        |
-//            | Z    | 50    |                        |
-//            +------+-------+------------------------+
 }
-
-
-
-
-
-
-
-
-
