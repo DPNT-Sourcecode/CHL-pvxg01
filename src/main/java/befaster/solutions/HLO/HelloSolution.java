484843c5ec74f8f4582fbf06cb4647df469d2d0c
sbx_user1051 <sbx_user1051@169.254.86.237>
2019-10-12 13:04:51 +0000

    Sat Oct 12 13:04:51 UTC 2019

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -1,10 +1,9 @@
 package befaster.solutions.HLO;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class HelloSolution {
     public String hello(String friendName) {
         return "Hello " + friendName;
     }
 }
+
 
---
+++ src/test/java/befaster/solutions/HLO/HloSolutionTest.java
@@ -0,0 +1,23 @@
+package befaster.solutions.HLO;
+
+import befaster.solutions.SUM.SumSolution;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HloSolutionTest {
+    private HelloSolution helloSolution;
+
+    @Before
+    public void setUp() {
+
+        helloSolution = new HelloSolution();
+    }
+
+    @Test
+    public void compute_sum() {
+        assertThat(helloSolution.hello("Fred"), equalTo("Hello Fred"));
+    }
+}
